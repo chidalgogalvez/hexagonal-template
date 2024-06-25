51e5bc785f676bcab4bedfce9513a54fa5ebf321
Christian Hidalgo <chidalgogalvez@gmail.com>
2024-06-25 10:26:54 -0400

    implement logger

--- src/modules/user/infraestructure/controller/user.controller.ts
+++ src/modules/user/infraestructure/controller/user.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { UserService } from '../../application/user.use-case';
+import { Logger } from '../../../../config/loggerConfig/AppLogger';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) { }
   @Get(':id')
   findOne(@Param('id') id: string) {
+    Logger.info(`UserController:findOne - id: ${id}`);
     return this.userService.findById(id);
   }
 }
